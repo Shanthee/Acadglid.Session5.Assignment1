fa5531b51991859525e459d448a971fb1eb0c429
Shanthee Sampath <shantheesam@gmail.com>
2017-01-03 10:46:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// declaration of variables
+var name = "shanthee";
+var age = 29;
+var dateofbirth = "17/Nov/1987";
+var placeofbirth = "hosur";
+
+//printing on console
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Date of Birth: " + dateofbirth);
+console.log("Place of Birth: " + placeofbirth);
